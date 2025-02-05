5f85a6ad7d98c06adef93e356f797dbb020e431a
Mr Guru <mytech@metrepairs.com>
2025-02-05 12:22:54 -0500

    Remove .history and update .gitignore

--- .history/app/page_20250205051130.tsx
+++
@@ -1,9 +0,0 @@
-import InventoryDashboard from './components/InventoryDashboard';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <InventoryDashboard />
-    </main>
-  );
-} 
